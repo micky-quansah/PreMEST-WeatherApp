d9932da64195cf969ea304155619a909751964c1
Micky <quansahmicheal@gmail.com>
2020-10-22 08:33:20 +0000

    working progress

--- weather-app/src/components/forms-component/Form.js
+++ weather-app/src/components/forms-component/Form.js
@@ -6,101 +6,81 @@ function Form(props) {
   const [submit, setSubmit] = useState(false);
   const [name, setName] = useState({value:""});
   const [password, setPassword] = useState({value:""});
-  const [input, setInput] = useState(false);
+  const [authPassword, setAuthPassword] = useState({value:""});
+  const [authName, setAuthName] = useState({value:""});
 
-  console.log(props.menuVisibility)
+  function Login() {
 
-  function handleSubmit(event) {
-    setSubmit({submit: true});
-    event.preventDefault();
+    const handleAuth = (e) => {
+      e.preventDefault();
+      if(name === authName && password === authPassword){
+        alert("LogIn was successful")
+        setSubmit({submit: true});
+      }
+    }
+
+    function handleAuthPassword(e) {
+      setAuthPassword({value: e.target.value})
+    }
+
+    function handleAuthName(e) {
+      setAuthName({value: e.target.value})
+    }
+  
+    return(<div>
+      <h1 className="signIn" > Log In</h1> 
+      <form className='hform' onSubmit={handleAuth}>
+      <label title="name">
+      <input id="name" value={authName.value} name="name" className='input input-text' type="text" placeholder="Name" onChange={handleAuthName}/><br />
+      </label>
+      <label title="name">
+      <input className='input input-text' name="password" value={authPassword.value} type="password" placeholder="Password" onChange={handleAuthPassword}/><br />
+      </label>
+      <input type="submit" value="Submit" onClick={handleAuth} />
+      </form>
+      </div>);
   }
 
-  function handlePassword(e) {
-    setPassword({value: e.target.value})
-  }
+  function SignUp() {
 
-  function handleName(e) {
-    setName({value: e.target.value})
-  }
-
-  function onInput() {
-    setInput({input: true});
+    function handleSubmit(event) {
+      event.preventDefault();
+      if(name >= 0 && password >= 0){
+        setSubmit({submit: true});
+      }
+    }
+  
+    function handlePassword(e) {
+      setPassword({value: e.target.value})
+    }
+  
+    function handleName(e) {
+      setName({value: e.target.value})
+    }
+  
+    return (<div>
+      <h1 className="signIn" >Sign Up</h1>
+      <form className='hform'>
+      <label title="name">
+      <input id="name" value={name.value} name="name" className='input input-text' type="text" placeholder="Name" onChange={handleName}/><br />
+      </label>
+      <label title="name">
+      <input className='input input-text' name="password" value={password.value} type="password" placeholder="Password" onChange={handlePassword}/><br />
+      </label>
+      <input type="submit" value="Submit" onClick={handleSubmit} />
+      </form>
+      </div>);
   }
 
   return (
     <section id='form-section' id="flyout" className={props.menuVisibility} >
     <img id="cancel" src={cancelLogo} onClick={props.onClick} />
       <div className="formContainer">
-      {submit ? <h1 id="signIn" > Log In</h1> : <h1 id="signIn" >Sign Up</h1> }
-      <form className='hform' onSubmit={handleSubmit}>
-        <label title="name">
-          <input id="name" value={name.value} name="name" className='input input-text' type="text" onClick={onInput} placeholder="Name" onChange={handleName} required/><br />
-        </label>
-        <label title="name">
-          <input className='input input-text' name="password" value={password.value} type="password" onClick={onInput} placeholder="Password" onChange={handlePassword} required/><br />
-        </label>
-        <input type="submit" value="Submit" onClick={handleSubmit} onMouseDown={props.handleMouseDown} />
-      </form>
-
+      {submit ? <Login/> : <SignUp/> }
       </div>
     </section>
   );
 
 }
-
-/* class Form extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {submit: false};
-    this.state = {name: " "};
-    this.state = {password: " "};
-    this.inputClick = false;
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleSubmit(event) {
-    this.setState({submit: true});
-    event.preventDefault();
-  }
-
-  handleChange(e) {
-    this.setState({[e.target.name]: e.target.value})
-  }
-
-  onInput() {
-    this.inputClick = true;
-  }
-  
-
-  render(){
-
-    this.visibility = "hide";
-
-    if (this.props.menuVisibility && this.inputClick===true) {
-      this.visibility = "show";
-    }
-
-    return (
-      <section id='form-section' id="flyout" className={this.visibility} >
-      <img id="cancel" src={cancelLogo} onMouseDown={this.props.handleMouseDown} />
-        <div className="formContainer">
-        {this.state.submit ? <h1 id="signIn" > Log In</h1> : <h1 id="signIn" >Sign Up</h1> }
-        <form className='hform' onSubmit={this.handleSubmit}>
-          <label title="name">
-            <input id="name" value={this.state.value} name="name" className='input input-text' type="text" onClick={this.onInput()} placeholder="Name" onChange={this.handleChange} required/><br />
-          </label>
-          <label title="name">
-            <input className='input input-text' name="password" value={this.state.value} type="password" onClick={this.onInput()} placeholder="Password" onChange={this.handleChange} required/><br />
-          </label>
-          <input type="submit" value="Submit" onClick={this.handleSubmit} onMouseDown={this.props.handleMouseDown} />
-        </form>
-
-        </div>
-      </section>
-    );
-  }
-} */
 
 export default Form;
